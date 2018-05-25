1931a653517becdba1a715d05738bc0585a7dcfb
Terence Mangram <tmangram@gmail.com>
2018-05-25 15:18:34 -0500

    Form displays form inputs unless loading reducer is triggered by form's onSubmit action

--- client/src/containers/Upload/Upload.js
+++ client/src/containers/Upload/Upload.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { addMaterial } from "../../store/actions/";
+import Spinner from "../../components/UI/Spinner";
 import TextFieldGroup from "../../components/UI/TextFieldGroup";
 import TextAreaFieldGroup from "../../components/UI/TextAreaFieldGroup";
 import SelectListGroup from "../../components/UI/SelectListGroup";
@@ -47,6 +48,51 @@ class Upload extends Component {
 			{ label: "Special Needs", value: "Special Needs" },
 			{ label: "High School", value: "High School" }
 		];
+		let form = (
+			<TextFieldGroup 
+				name="title"
+				placeholder="* Title (Eg. Battleship - Marvel Characters Version)"
+				value={this.state.title}
+				error={errors.title}
+				onChange={this.onChange}
+			/>
+			<TextAreaFieldGroup
+				name="instructions"
+				placeholder="* Detailed instructions"
+				value={this.state.instructions}
+				error={errors.instructions}
+				info="Instructions should explain the basic use of the exercise."
+				onChange={this.onChange}
+			/>
+			<SelectListGroup
+				name="grade"
+				value={this.state.grade}
+				error={errors.grade}
+				options={options}
+				info="What grade level is this exercise for?"
+				onChange={this.onChange}
+			/>
+			<div className="input-group mb-3">
+			  <div className="input-group-prepend">
+			    <span className="input-group-text">Upload</span>
+			  </div>
+			  <div className="custom-file">
+			    <input 
+			    name="fileUpload" 
+			    type="file" 
+			    className="custom-file-input" 
+			    id="fileUpload" 
+			    onChange={this.onChange} />
+			    <label className="custom-file-label" htmlFor="fileUpload">
+				    {this.state.fileUpload}
+			    </label>
+			  </div>
+			</div>
+			<input type="submit" className="btn btn-info btn-block mt-4" />
+		);
+		if(this.props.loading) {
+			form = <Spinner />;
+		}
 		return (
 			<div className="container">
 				<div className="row">
@@ -54,46 +100,7 @@ class Upload extends Component {
 						<h2 className="text-center">Upload A File</h2>
 						<p className="lead text-center">People helping people, it's a beautiful thing</p>
 						<form onSubmit={this.onSubmit}>
-							<TextFieldGroup 
-							name="title"
-							placeholder="* Title (Eg. Battleship - Marvel Characters Version)"
-							value={this.state.title}
-							error={errors.title}
-							onChange={this.onChange}
-							/>
-							<TextAreaFieldGroup
-								name="instructions"
-								placeholder="* Detailed instructions"
-								value={this.state.instructions}
-								error={errors.instructions}
-								info="Instructions should explain the basic use of the exercise."
-								onChange={this.onChange}
-							/>
-							<SelectListGroup
-								name="grade"
-								value={this.state.grade}
-								error={errors.grade}
-								options={options}
-								info="What grade level is this exercise for?"
-								onChange={this.onChange}
-							/>
-							<div className="input-group mb-3">
-							  <div className="input-group-prepend">
-							    <span className="input-group-text">Upload</span>
-							  </div>
-							  <div className="custom-file">
-							    <input 
-							    name="fileUpload" 
-							    type="file" 
-							    className="custom-file-input" 
-							    id="fileUpload" 
-							    onChange={this.onChange} />
-							    <label className="custom-file-label" htmlFor="fileUpload">
-								    {this.state.fileUpload}
-							    </label>
-							  </div>
-							</div>
-							<input type="submit" className="btn btn-info btn-block mt-4" />
+							{form}
 						</form>
 					</div>
 				</div>
@@ -109,7 +116,8 @@ Upload.propTypes = {
 
 const mapStateToProps = state => ({
 	auth: state.auth,
-	errors: state.errors
+	errors: state.errors,
+	loading: state.loading
 });
 
 const mapDispatchToProps = dispatch => ({
