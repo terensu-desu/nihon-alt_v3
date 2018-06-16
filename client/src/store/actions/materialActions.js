eb6522c3f6ab1bf7f5d1b616402eea1574a6d5cf
Terence Mangram <tmangram@gmail.com>
2018-06-16 03:05:48 -0500

    Add upload_start to reset success state

--- client/src/store/actions/materialActions.js
+++ client/src/store/actions/materialActions.js
@@ -4,6 +4,7 @@ import { startLoading, endLoading } from "./loadingActions";
 
 export const addMaterial = newMaterial => dispatch => {
 	dispatch(startLoading());
+	dispatch({ type: types.UPLOAD_START });
 	axios({
 		method: "post",
 		url: "/api/materials/",
@@ -15,10 +16,11 @@ export const addMaterial = newMaterial => dispatch => {
 		.then(res => {
 			dispatch(endLoading());
 			dispatch({ type: types.CLEAR_ERRORS	});
-			console.log("[RESPONSE AT REDUX] ", res);
+			dispatch({ type: types.UPLOAD_SUCCESS });
 		})
 		.catch(err => {
 			dispatch(endLoading());
+			dispatch({ type: types.UPLOAD_FAIL });
 			dispatch({
 				type: types.GET_ERRORS,
 				payload: err.response.data
