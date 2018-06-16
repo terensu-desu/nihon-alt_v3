5be9a1a4130e5f5fba19ad810a0e3498ffc6f893
Terence Mangram <tmangram@gmail.com>
2018-06-16 03:06:40 -0500

    Add upload_start to reset success state

--- client/src/store/reducers/uploadReducer.js
+++ client/src/store/reducers/uploadReducer.js
@@ -1,9 +1,17 @@
-//import * as types from "../actions/actionTypes";
+import * as types from "../actions/actionTypes";
 
-const initalState = {};
+const initalState = {
+	uploadSuccess: false
+};
 
 const reducer = (state = initalState, action) => {
 	switch(action.type) {
+		case types.UPLOAD_START:
+			return { uploadSuccess: false };
+		case types.UPLOAD_SUCCESS:
+			return { uploadSuccess: true };
+		case types.UPLOAD_FAIL:
+			return { uploadSuccess: false };
 		default:
 			return state;
 	}
