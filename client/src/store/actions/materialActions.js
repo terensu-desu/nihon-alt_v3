ce93cf0ef6140c927eb39cc93aa86b4f0fba6feb
Terence Mangram <tmangram@gmail.com>
2018-06-16 02:24:01 -0500

    Clear errors on succussful post, end loading on either result

--- client/src/store/actions/materialActions.js
+++ client/src/store/actions/materialActions.js
@@ -4,16 +4,24 @@ import { startLoading, endLoading } from "./loadingActions";
 
 export const addMaterial = newMaterial => dispatch => {
 	dispatch(startLoading());
-	console.log("[FROM REDUX ACTION]", newMaterial);
-	axios.post("/api/materials/", newMaterial)
+	axios({
+		method: "post",
+		url: "/api/materials/",
+		data: newMaterial,
+		config: {
+			headers: { "Content-Type": "multipart/form-data" }
+		}
+	})
 		.then(res => {
 			dispatch(endLoading());
-			console.log(res);
+			dispatch({ type: types.CLEAR_ERRORS	});
+			console.log("[RESPONSE AT REDUX] ", res);
 		})
 		.catch(err => {
+			dispatch(endLoading());
 			dispatch({
 				type: types.GET_ERRORS,
 				payload: err.response.data
-			})
+			});
 		});
-}
+};
