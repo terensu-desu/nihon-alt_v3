7215ec8f809553ab41166bc3bde6f6841451f48f
Terence Mangram <tmangram@gmail.com>
2018-06-18 10:31:47 -0500

    Add footer element

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const footer = props => (
+	<div className="footer">
+		<div className="container">
+			<div className="row mt-4">
+				<div className="col-md-12">
+					<p className="text-center footer-text">
+						Created by <a href="https://www.linkedin.com/in/terence-mangram/">Terence Mangram</a> with React, Redux, NodeJS, Express, MongoDB, and Bootstrap 4. 
+						Learn more at <a href="https://github.com/terensu-desu/nihon-alt_v3">GitHub</a>.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default footer;
