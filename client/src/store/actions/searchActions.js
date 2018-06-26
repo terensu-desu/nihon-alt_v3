932a94fa7397105416d7da1d7b83363977b07138
Terence Mangram <tmangram@gmail.com>
2018-06-26 15:56:55 -0500

    Action calls search API, returns response and query to reducer and redirects app

---
+++ client/src/store/actions/searchActions.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import * as types from "./actionTypes";
+import { startLoading, endLoading } from "./loadingActions";
+
+export const handleSearch = (query, history) => dispatch => {
+	dispatch(startLoading());
+	axios.post("/api/materials/search", query)
+		.then(res => {
+			const payload = {
+				results: res.data,
+				query: query.query
+			}
+			dispatch(endLoading());
+			dispatch({
+				type: types.HANDLE_SEARCH,
+				payload: payload
+			});
+			history.push("/searchresults");
+		})
+		.catch(err => {
+			dispatch(endLoading());
+			console.log("error in handle search")
+		});
+}
---
+++ client/src/store/reducers/searchReducer.js
@@ -0,0 +1,21 @@
+import * as types from "../actions/actionTypes";
+
+const initialState = {
+	query: "",
+	results: []
+};
+
+const reducer = (state = initialState, action) => {
+	switch(action.type) {
+		case types.HANDLE_SEARCH:
+			return {
+				...state,
+				query: action.payload.query,
+				results: action.payload.results
+			}
+		default:
+			return state;
+	}
+};
+
+export default reducer;
