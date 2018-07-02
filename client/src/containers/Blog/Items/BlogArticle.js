9b588fc0b490a0fab9b275781f4f1b0150484326
Terence Mangram <tmangram@gmail.com>
2018-07-02 00:00:42 -0500

    Add Like component, add and remove article like redux actions and auth props

--- client/src/containers/Blog/Items/BlogArticle.js
+++ client/src/containers/Blog/Items/BlogArticle.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux"
 import Moment from "react-moment";
-import { retrieveArticle } from "../../../store/actions/";
+import { 
+	retrieveArticle,
+	addArticleLike,
+	removeArticleLike } from "../../../store/actions/";
+import Likes from "../../../components/UI/Likes";
 
 class BlogArticle extends Component {
 	componentDidMount() {
 		this.props.onRetrieveArticle(this.props.match.params.id)
 	}
+	onLikeClick = itemId => {
+		this.props.onAddLike(itemId);
+	};
+	onUnlikeClick = itemId => {
+		this.props.onRemoveLike(itemId);
+	};
+	findUserLikes = likes => {
+		const {authUser} = this.props;
+		if(likes.filter(like => like.user === authUser.id).length > 0) {
+			return true;
+		} else { return false }
+	};
 	render() {
-		const { article } = this.props;
+		const { article, authStatus } = this.props;
 		return (
 			<div className="container">
 				<div className="row">
@@ -19,7 +35,13 @@ class BlogArticle extends Component {
 						    <h6 className="card-subtitle mb-2 text-muted">by {article.name}</h6>
 						    <small><Moment format="YYYY/MM/DD">{article.date}</Moment></small>
 						    <p className="card-text">{article.content}</p>
-						    <a href="#!" className="card-link">Comments/Likes</a>
+						    <p className="card-text">Like this article?</p>
+						    <Likes 
+						    auth={authStatus}
+				      	likes={article.likes ? article.likes.length : 0}
+				      	onLikeClick={() => this.onLikeClick(article._id)}
+				      	onUnlikeClick={() => this.onUnlikeClick(article._id)}
+				      	findUserLikes={() => this.findUserLikes(article.likes)} />
 						  </div>
 						</div>
 					</div>
@@ -30,11 +52,15 @@ class BlogArticle extends Component {
 };
 
 const mapStateToProps = state => ({
+	authStatus: state.auth.isAuthenticated,
+	authUser: state.auth.user,
 	article: state.blog.targetArticle
 });
 
 const mapDispatchToProps = dispatch => ({
-	onRetrieveArticle: id => dispatch(retrieveArticle(id))
+	onRetrieveArticle: id => dispatch(retrieveArticle(id)),
+	onAddLike: id => dispatch(addArticleLike(id)),
+	onRemoveLike: id => dispatch(removeArticleLike(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(BlogArticle);
