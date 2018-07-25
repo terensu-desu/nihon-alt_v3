3bc4611ded02afe28c2ff8bcef2d72f582a990b8
Terence Mangram <tmangram@gmail.com>
2018-07-25 14:31:32 -0500

    Add function for intiializing alert list from backend api

--- client/src/store/actions/adminActions.js
+++ client/src/store/actions/adminActions.js
@@ -1,8 +1,22 @@
 import * as types from "./actionTypes";
 import axios from "axios";
 
+
+export const initializeList = () => dispatch => {
+	axios.get(`/api/admin/init/`)
+		.then(res => {
+			dispatch({
+				type: types.INITIALIZE_LIST,
+				payload: res.data
+			});
+		})
+		.catch(err => {
+			console.log(err);
+		});
+};
+
 export const initializeComment = (articleId, commentId) => dispatch => {
-	axios.get(`/api/posts/comment/${articleId}/${commentId}`)
+	axios.get(`/api/admin/comment/${articleId}/${commentId}`)
 		.then(res => {
 			dispatch({
 				type: types.INITIALIZE_COMMENT,
